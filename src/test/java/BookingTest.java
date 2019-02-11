fe6fa9c5e7623dfbab6072ab41649a9e6b4c708f
RSa <rakushynserhii@gmail.com>
2019-02-11 21:21:22 +0200

    test

--- src/test/java/BookingTest.java
+++ src/test/java/BookingTest.java
@@ -18,7 +18,7 @@ public class BookingTest extends BaseTest {
 
     @Test
     public void testOpenSite() throws InterruptedException {
-        driver.get("https://booking.uz.gov.ua/ru/?from=2200001&to=2218000&date=2019-02-09&time=00%3A00&url=train-list");
+        driver.get("https://booking.uz.gov.ua/ru/?from=2200001&to=2218000&date=2019-02-11&time=00%3A00&url=train-list");
         HomePage homePage = new HomePage(driver);
 //        homePage.setFrom("Киев");
 //        homePage.setTo("Львов");
